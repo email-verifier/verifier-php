9a8dc520aaef42b28ca98c799cf9334b6bfa23c2
MeetChopra <31788849+MeetChopra@users.noreply.github.com>
2019-02-12 20:24:26 +0530

    Create verify.php

---
+++ verify.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * verify email from curl request
+ * https://github.com/email-verifier/verifier-php
+ */
+
+public function verifyEmail($email = null, $access_token, $details = false)
+{
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://verifier.meetchopra.com/verify/'.$email.'?token='.$access_token);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    }
+    curl_close ($ch);
+
+    if ($details) {
+        return json_decode($result)
+    }else{
+        $data = json_decode($result);
+        return $data['status']
+    }
+}
+
+
+?>
